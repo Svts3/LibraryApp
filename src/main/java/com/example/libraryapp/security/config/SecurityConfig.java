12038d21f1110e5e07e65391a11f8b06666bfcf3
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2024-05-01 23:32:20 +0300

    Add oauth2 support

---
+++ src/main/java/com/example/libraryapp/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.libraryapp.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request->{
+            request.anyRequest().authenticated();
+        });
+        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client(Customizer.withDefaults());
+        return http.build();
+    }
+
+
+}
