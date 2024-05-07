1df4c8bdb8e6c5dc27207b9cb9dba09acfa3bcfd
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2024-05-08 02:01:49 +0300

    Set up resource server

--- src/main/java/com/example/libraryapp/security/config/SecurityConfig.java
+++ src/main/java/com/example/libraryapp/security/config/SecurityConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,11 +11,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request->{
+        http.authorizeHttpRequests(request -> {
             request.anyRequest().authenticated();
         });
         http.oauth2Login(Customizer.withDefaults());
         http.oauth2Client(Customizer.withDefaults());
+        http.oauth2ResourceServer(resourceServerConfig -> {
+            resourceServerConfig.jwt(Customizer.withDefaults());
+        });
         return http.build();
     }
 
